17494830306672d6d1f25ac0455e9e5e6970eff3
Jose Manuel <josemanuel0709@hotmail.es>
2021-08-19 10:06:39 +0200

    Componente Header, Task-List y Task-item creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskItemComponent } from './task-list/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
