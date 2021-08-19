f9ebfaa33a78a708d80cbbd21e6bb8c88777f1c1
Jose Manuel <josemanuel0709@hotmail.es>
2021-08-19 11:49:55 +0200

    Bootstrap añadido. Estructura general.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,17 @@ import { HeaderComponent } from './header/header.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskItemComponent } from './task-list/task-item/task-item.component';
 
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TaskListComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
